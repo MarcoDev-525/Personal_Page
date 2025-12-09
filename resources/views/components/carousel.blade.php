35e51382f1d2dab611ad97a52efd91d239a7be9a
MarcoDev-525 <marco.fasano525@gmail.com>
2025-12-09 08:35:12 +0100

    modifica e fix del carosello

---
+++ resources/views/components/carousel.blade.php
@@ -0,0 +1,22 @@
+<div class="carosello"  style="width: auto; ">
+<div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+<img src="https://picsum.dev//300/200" alt="Random Image" >    
+</div>
+    <div class="carousel-item">
+<img src="https://picsum.dev//300/200" alt="">    
+</div>
+    <div class="carousel-item">
+<img src="https://picsum.dev//300/200" alt="">   
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+</div>
