6908908cb132e402b7e4bca3601ccb9b2d9f4157
MarcoDev-525 <marco.fasano525@gmail.com>
2025-12-09 11:42:11 +0100

    rimozione dell'alt nel carosello

--- resources/views/components/carousel.blade.php
+++ resources/views/components/carousel.blade.php
@@ -2,13 +2,13 @@
 <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-<img src="https://picsum.dev//300/200" alt="Random Image" >    
+<img src="https://picsum.photos/200/300" alt="" >    
 </div>
     <div class="carousel-item">
-<img src="https://picsum.dev//300/200" alt="">    
+<img src="https://picsum.photos/200/300" alt="">    
 </div>
     <div class="carousel-item">
-<img src="https://picsum.dev//300/200" alt="">   
+<img src="https://picsum.photos/200/300" alt="">   
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
